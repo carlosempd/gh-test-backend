5d794650205adaf51cc4a05bb1f1e191a501cad7
Carlos <carlosempd@gmail.com>
2023-08-22 20:12:41 -0400

    ✨ feat: create commits module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CommitsModule } from './commits/commits.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    CommitsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/commits/commits.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { CommitsService } from './commits.service';
+
+@Controller('commits')
+export class CommitsController {
+	constructor(
+		private readonly commitsService: CommitsService
+	) {}
+
+	@Get()
+	findAll() {
+		return this.commitsService.getAll();
+	}
+}
